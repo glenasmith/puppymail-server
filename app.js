b705c3e2dfb302e396a91451655affd54ac5a8f0
Glen Smith <glen@bytecode.com.au>
2016-10-19 16:45:11 +1100

    Proxy server to avoid CORS issues

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var proxy = require('express-http-proxy');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(allowCrossDomain);
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/proxy', proxy('https://getpocket.com/', {
+
+  decorateRequest: function(proxyReq, originalReq) {
+    // you can update headers
+    //proxyReq.headers['Content-Type'] = 'text/html';
+    // you can change the method
+    //proxyReq.method = 'GET';
+    // you can munge the bodyContent.
+    //proxyReq.bodyContent = proxyReq.bodyContent.replace(/losing/, 'winning!');
+    console.log(proxyReq);
+    return proxyReq;
+  }
+}));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
